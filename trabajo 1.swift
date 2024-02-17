fa68bd80d79115a0c39a236dd78fc6eb8e0ea700
antivirusnn <159796981+antivirusnn@users.noreply.github.com>
2024-02-17 13:27:55 -0600

    chucha
    profe me acabo de dar cuenta que esto nunca se subio, porfavor aceptelo

---
+++ trabajo 1.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+/*
+comentarios en varias lineas
+*/
+//variables
+//Ojo vamos a usar estandar Camel para nombrarlas 
+//En swift usamos VAR para declarar una variables
+
+var myFirstWork = "Hola CBTiS"
+
+print(myFirstWork)
+//constantes se definen con la palabra reservada LET y el nombre con la constante
+ let idInternational = "+52"
+ print(idInternational)
+// idInternational= 5634815970
